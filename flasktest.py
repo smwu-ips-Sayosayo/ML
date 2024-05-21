5110e42e56536d15fce8adcc3eafa01d29fd7f7d
Yongeun <gdakate1215@gmail.com>
2024-05-21 17:22:10 +0900

    서버 연결 테스트용 코드

--- flasktest.py
+++ flasktest.py
@@ -3,6 +3,7 @@ from flask import Flask, Response, request, json, jsonify
 
 import cv2
 import camera2
+import io
 
 app = Flask(__name__)
 @app.route('/stream', methods=['POST'])
@@ -13,6 +14,10 @@ def stream():
     frame = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
     # 객체 감지 , 손 인식
     processed_frame = camera2.process_stream(frame)
+
+    _, buffer = cv2.imencode('.jpg', processed_frame)
+    buffer = io.BytesIO(buffer)
+
     # 결과 반환
     response = {
         "result": "카메라 실행 완료"
@@ -27,7 +32,20 @@ def versionCheck():
         "result": "ok"
     }
     return jsonify(response)
+from flask import Flask, request
 
+app = Flask(__name__)
 
-if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5000)
+@app.route('/video', methods=['POST'])
+def receive_image():
+    try:
+        image_data = request.get_data()
+        # 여기서 image_data를 사용하여 이미지 처리를 수행할 수 있습니다.
+
+        # 연결 확인 응답을 보냅니다.
+        return '연결됨', 200
+    except Exception as e:
+        return f'에러: {e}', 500
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
